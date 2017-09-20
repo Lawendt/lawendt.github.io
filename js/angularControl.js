ab45b6a3b6296d6e4ba98e7fbd18a909c5e8019c
Luiz Wendt <luizaugustow_1@msn.com>
2017-09-20 20:28:24 -0300

    Few changes on the padding and margin. Added top projects list

--- js/angularControl.js
+++ js/angularControl.js
@@ -1,40 +1,46 @@
 var app = angular.module('myApp', ["ngRoute"]);
 
 app.config(function($routeProvider) {
-   $routeProvider
-	   .when("/", {
-      templateUrl: "portfolio.html"
-   })
+    $routeProvider
+        .when("/", {
+        templateUrl: "portfolio.html"
+    })
         .when("/about", {
-       templateUrl: "about.html"
-   })
-	   .otherwise({
-      template: "<b>Opção inválida!</b>"
-   });
+        templateUrl: "about.html"
+    })
+        .otherwise({
+        template: "<b>Opção inválida!</b>"
+    });
 });
 
 app.controller('myCtrl', function($scope) {
     $scope.projects = Projects;
+    $scope.topProjects = TopProjects;
     $scope.projectFilter = Projects;
-    $scope.currentTagFilter = "Everything";
+    $scope.currentTagFilter = "Top";
     $scope.filterBy = function(tag) {
         tag = tag.tag;
         console.log(tag);
         if(tag == null || tag == undefined)
         {
-            $scope.projectFilter = $scope.projects;
-            $scope.currentTagFilter = "Everything";
-            return;
+            $scope.projectFilter = $scope.topProjects;
+            $scope.currentTagFilter = "Top";
         }
-        $scope.currentTagFilter = tag;
-        $scope.projectFilter = [];
-        for (var p in $scope.projects){
-            console.log(p);
-            for(var t in $scope.projects[p].tags) {
-                
-                if(tag ==  $scope.projects[p].tags[t]){
-                    $scope.projectFilter.push($scope.projects[p]);
-                    break;
+        else if(tag == "Everything"){
+            $scope.projectFilter = $scope.projects;
+            $scope.currentTagFilter = tag;
+        }
+        else{
+            $scope.currentTagFilter = tag;
+            $scope.projectFilter = [];
+            for (var p in $scope.projects){
+                console.log(p);
+                for(var t in $scope.projects[p].tags) {
+
+                    if(tag ==  $scope.projects[p].tags[t]){
+                        $scope.projectFilter.push($scope.projects[p]);
+                        break;
+                    }
                 }
             }
         }
