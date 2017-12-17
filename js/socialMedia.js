c110d3012f877fdda41648336c0ebb2ef76c2b42
Luiz Wendt <luizaugustow_1@msn.com>
2017-12-17 20:03:56 -0200

    icons now change color

---
+++ js/socialMedia.js
@@ -0,0 +1,32 @@
+function onHoverTwitter(element) {
+    element.setAttribute('src', './img/icons/twitter-logo_color.png');
+}
+
+function onHoverItch(element) {
+    element.setAttribute('src', './img/icons/itchio-logo_color.png');
+}
+
+function onHoverFacebook(element) {
+    element.setAttribute('src', './img/icons/facebook-logo_color.png');
+}
+
+function onHoverLinked(element) {
+    element.setAttribute('src', './img/icons/linkedin-logo_color.png');
+}
+
+
+function onUnhoverTwitter(element) {
+    element.setAttribute('src', './img/icons/twitter-logo.png');
+}
+
+function onUnhoverItch(element) {
+    element.setAttribute('src', './img/icons/itchio-logo.png');
+}
+
+function onUnhoverFacebook(element) {
+    element.setAttribute('src', './img/icons/facebook-logo.png');
+}
+
+function onUnhoverLinked(element) {
+    element.setAttribute('src', './img/icons/linkedin-logo.png');
+}
