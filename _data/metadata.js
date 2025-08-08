d7eed89ce31db6d6441108f04f281e2dfd521b4c
Luiz Wendt <luiz.wendt@outlook.com>
2025-08-08 13:04:35 -0300

    start with elleventy

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Lawendt",
+	url: "https://lawendt.github.io/",
+	language: "en",
+	description: "Luiz Wendt makes games and other things, maybe",
+	author: {
+		name: "Luiz A. Wendt",
+		email: "luizawendt@gmail.com",
+		url: "https://lawendt.github.io/about/"
+	}
+}
